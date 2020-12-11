851d9ddd1e2ff10bde37ef7f42969c010ad8d733
lhugens <leonardolobato@protonmail.com>
2020-12-11 20:17:16 +0100

    getArgs

--- haskell/first.hs
+++ haskell/first.hs
@@ -1,7 +1,16 @@
+module Main where
+import System.Environment
+import System.IO
+
 fac :: Integer -> Integer
 fac n = if n == 0 then 1 else n * fac (n-1)
 
+prompt = do
+    n <- getLine
+    return n
+
 main :: IO()
 main = do
-    print (fac 3 :: Integer)
-
+--    n <- prompt
+    args <- getArgs
+    putStrLn ("Hello, " ++ args !! 0)
