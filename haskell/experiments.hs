1ca6d68908c612907d645d76a7cc48024c466594
lhugens <leonardolobato@protonmail.com>
2020-12-12 21:07:42 +0100

    implemented newton-raphson methon

--- haskell/experiments.hs
+++ haskell/experiments.hs
@@ -11,3 +11,19 @@ andd xs = foldr (&&) True xs
 
 orr :: [Bool] -> Bool
 orr xs = foldr (||) False xs
+
+-- newton-raphson method
+
+next :: Float -> Float -> Float
+next n x = (x + n/x)/2
+
+repeatt :: (a -> a) -> a -> [a] 
+repeatt f x = f(x) : repeatt f (f(x)) 
+
+within :: Float -> [Float] -> Float
+within eps (a:b:xs)
+  | (a-b) < eps = b
+  | otherwise = within eps (b:xs)
+
+sqrtt :: Float -> Float -> Float -> Float
+sqrtt a0 n eps = within eps ( repeatt (next n) a0 )
