fcb5f974eb86cbbc46bd05c8508e0ef241442363
lhugens <leonardolobato@protonmail.com>
2020-12-12 16:44:00 +0100

    tut 5

--- haskell/tut.hs
+++ haskell/tut.hs
@@ -1,6 +1,10 @@
+-- tutorial 1
+
 sumer :: [Float] -> Float
 sumer [] = 0
 sumer (x:xs) = x + sum xs
+
+-- tutorial 2
 
 in_range :: Float -> Float -> Float -> Bool
 in_range min max x = min <= x && x <= max
@@ -15,7 +19,9 @@ in_range3 :: Integer -> Integer -> Integer -> Bool
 in_range3 min max x = lower && higher
     where lower = min <= x
           higher = x <= max
-    
+
+-- tutorial 3
+
 fac :: Integer -> Integer
 fac 0 = 1
 fac n = n * fac (n-1)
@@ -25,4 +31,52 @@ fac2 n
     | n <= 1 = 1
     | otherwise = n * fac2 (n-1)
 
+fac3 :: Integer -> Integer
+fac3 n = aux n 1
+    where 
+        aux n acc
+            | n <= 1 = acc
+            | otherwise = aux (n-1) n*acc
+
+-- tutorial 4
+
+asc :: Int -> Int -> [Int]
+asc n m 
+  | n > m = []
+  | n == m = [m]
+  | n < m = n : asc (n+1) m
+
+-- import Data.List
+-- head [1,2,3,4] => 1
+-- tail [1,2,3,4] => [2,3,4]
+-- init [1,2,3,4] => [1,2,3]
+-- length [1,2,3,4] => 4
+-- null [] => True
+-- null [9] => False
+-- and [True, False] -> False
+-- or [True, False] -> True
+-- [ 2*x | x <- [1, 2, 3], x > 1] => [4,6]
+
+evens :: [Int] -> [Int]
+evens [] = []
+evens (x:xs)
+  | mod x 2 == 0 = x : evens xs
+  | otherwise = evens xs
+
+addTuples :: [(Int, Int)] -> [Int]
+addTuples xs = [ x+y | (x,y) <- xs]
+
+-- tutorial 5
+
+elems :: (Eq a) => a -> [a] -> Bool
+elems _ [] = False
+elems a (x:xs)
+  | x == a = True
+  | otherwise = elems a xs
+
+elems2 :: (Eq a) => a -> [a] -> Bool
+elems2 _ [] = False
+elems2 a (x:xs) = (x == a) || elems2 a xs
+
+nub :: (Eq a) => [a] -> [a]
 
